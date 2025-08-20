f1ffdeb80b41cbe949729ccb2c1aa806f2c8a9d3
sagarjaid <sagarjaid321@gmail.com>
2025-08-20 22:14:43 +0530

    button padding fix

--- components/BookerDemo.tsx
+++ components/BookerDemo.tsx
@@ -15,7 +15,7 @@ export default function BookerDemo({
     <Link
       target="_blank"
       href="https://calendar.google.com/calendar/appointments/schedules/AcZssZ0dlFbpOGz632w39CFnxBTVwjC_q7qdR3T6WU2aoV9GbJqe7F1v5hsfBq6ldDwWXjc3MPHN8sof"
-      className="bg-black border p-2 text-center w-fit text-white text-sm font-bold px-4 rounded-sm"
+      className="bg-black border p-1.5 text-center w-fit text-white text-sm font-bold px-4 rounded-sm"
     >
       <div className="flex items-center gap-2">
         {buttonText}
