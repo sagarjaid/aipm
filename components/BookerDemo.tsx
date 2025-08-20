7baa619a94d83d0f12b1bd47497d2418b1842039
sagarjaid <sagarjaid321@gmail.com>
2025-08-20 22:07:11 +0530

    Book demo simplified

--- components/BookerDemo.tsx
+++ components/BookerDemo.tsx
@@ -1,19 +1,25 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
+import Link from "next/link";
+import {  CircleChevronRight, CornerRightUp, Zap } from "lucide-react";
 
 interface BookerDemoProps {
   buttonText?: string;
 }
 
-export default function BookerDemo({  buttonText = 'BOOK A DEMO' }: BookerDemoProps) {
+
+export default function BookerDemo({
+  buttonText = "BOOK A DEMO",
+}: BookerDemoProps) {
   return (
-    <Link 
-    target='_blank'
+    <Link
+      target="_blank"
       href="https://calendar.google.com/calendar/appointments/schedules/AcZssZ0dlFbpOGz632w39CFnxBTVwjC_q7qdR3T6WU2aoV9GbJqe7F1v5hsfBq6ldDwWXjc3MPHN8sof"
-      className='bg-black border p-2 text-center w-fit text-white text-sm font-bold px-4 rounded-sm'
+      className="bg-black border p-2 text-center w-fit text-white text-sm font-bold px-4 rounded-sm"
     >
-      {buttonText}
+      <div className="flex items-center gap-2">
+        {buttonText}
+      </div>
     </Link>
   );
 }
